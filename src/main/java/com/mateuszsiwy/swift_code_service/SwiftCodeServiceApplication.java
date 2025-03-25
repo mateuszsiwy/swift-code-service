352d10745e70854cf464b8bdccaa850015f67815
msiwy12345 <mateuszsiwy00@gmail.com>
2025-03-25 20:02:33 +0100

    Initial commit

---
+++ src/main/java/com/mateuszsiwy/swift_code_service/SwiftCodeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mateuszsiwy.swift_code_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftCodeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiftCodeServiceApplication.class, args);
+	}
+
+}
